d822fbf7cdfe80d97eef2869fcb2e6dc6ff78a5a
goldds96 <86287862+goldds96@users.noreply.github.com>
2022-11-10 11:02:40 +0900

    Update 2480-주사위-세개.c

--- 단계별로 풀어보기/2. 조건문/2480-주사위-세개.c
+++ 단계별로 풀어보기/2. 조건문/2480-주사위-세개.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(void)
+{
+    int num1, num2, num3, max;
+    scanf("%d %d %d", &num1, &num2, &num3);
+
+    if ((num1 == num2) && (num2 == num3))
+      printf("%d", 10000 + num1 * 1000);
+
+    else if ((num1 == num2) || (num1 == num3))
+      printf("%d", 1000 + num1 * 100);
+    
+    else if (num2 == num3)
+      printf("%d", 1000 + num2 * 100);
+
+    else
+    {
+      num1 > num2 ? (max = num1) : (max = num2);
+      max > num3 ? (max = max) : (max = num3);
+      printf("%d", 100 * max);
+    }
+    return 0;
+}
